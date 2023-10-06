6d854f3a68a6f95dc61a35bdd6e19f08ad7458fc
Falllll <faldiharido@gmail.com>
2023-10-06 13:50:03 +0700

    setup model relation and add order_product table to migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,20 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function reviews()
+    {
+        return $this->belongsTo(Review::class);
+    }
 }
